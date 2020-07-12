c435f041e4edbef23ceb5365c2b9752df540516a
Stan van der Kroon <Stan.vdKroon@ziggo.nl>
2020-07-12 14:11:24 +0200

    only players that are op can send and server can send now

--- minecraftTestPlugin/src/me/test/testplugin/Main.java
+++ minecraftTestPlugin/src/me/test/testplugin/Main.java
@@ -1,6 +1,7 @@
 package me.test.testplugin;
 
 import java.text.MessageFormat;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -14,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class Main extends JavaPlugin implements Listener {
 	
@@ -32,47 +34,54 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(label.equalsIgnoreCase("show")) {
-			if(sender instanceof Player) {
-				Player p = (Player) sender;
-				World w = p.getWorld();
-				String msg = "";
-				
-				for (Chunk c : w.getForceLoadedChunks()) 
-					msg += "\nx: " +  String.valueOf(c.getX()) + ", z: "+  String.valueOf(c.getZ());
-				
-				if (msg != "")
-					p.sendMessage("Chunk(s) force loaded at:" + msg);
-				else 
-					p.sendMessage("No chunks are force loaded");
-				
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			if (!p.isOp()) {
+				p.sendMessage("You do not have permission to use this command!");
 				return true;
 			}
+				
+		}
+		
+		if(label.equalsIgnoreCase("show")) {
+			List<World> worlds = sender.getServer().getWorlds();
+			String msg = "";
+			boolean chunksLoaded = false;
+			for (World w : worlds) { 
+				chunksLoaded = false;
+//				msg += "\n  " + w.getName() + " at:";
+				for (Chunk c : w.getForceLoadedChunks()) { 
+					msg += "\n    x: " +  String.valueOf(c.getX()) + ", z: "+  String.valueOf(c.getZ());
+					chunksLoaded = true;
+				}
+				if (chunksLoaded)
+					msg = "\n  " + w.getName() + " at:" + msg;
+			}
+			
+			if (msg != "")
+				sender.sendMessage("Chunk(s) force loaded in:" + msg);
+			else 
+				sender.sendMessage("No chunks are force loaded");
+			
+			return true;
+
 		}
 		
 		if(label.equalsIgnoreCase("del")) {
-			if(sender instanceof Player) {
-				Player p = (Player) sender;
-				if (args.length != 2) {
-					p.sendMessage("Please give the x and z coordinate of the chunk.");
-					return true;
-				}
-				Bukkit.getWorld("world").setChunkForceLoaded(Integer.parseInt(args[0]), Integer.parseInt(args[1]), false);
-				p.sendMessage((String) MessageFormat.format("Unloaded chunk at coordinates: x:{0}, z:{1}", args[0], args[1]));
+			if (args.length != 2) {
+				sender.sendMessage("Please give the x and z coordinate of the chunk.");
 				return true;
 			}
+			Bukkit.getWorld("world").setChunkForceLoaded(Integer.parseInt(args[0]), Integer.parseInt(args[1]), false);
+			sender.sendMessage((String) MessageFormat.format("Unloaded chunk at coordinates: x:{0}, z:{1}", args[0], args[1]));
+			return true;
 		}
 		
 		if(label.equalsIgnoreCase("delAll")) {
-			if(sender instanceof Player) {
-				Player p = (Player) sender;
-				
-				World w = p.getWorld();
-				for (Chunk c : w.getForceLoadedChunks())
-					Bukkit.getWorld("world").setChunkForceLoaded(c.getX(), c.getZ(), false);
-				p.sendMessage("No more chunks are force loaded");
-				return true;
-			}
+			for (Chunk c : Bukkit.getWorld("world").getForceLoadedChunks())
+				Bukkit.getWorld("world").setChunkForceLoaded(c.getX(), c.getZ(), false);
+			sender.sendMessage("No more chunks are force loaded");
+			return true;
 		}
 		return false;
 	}
